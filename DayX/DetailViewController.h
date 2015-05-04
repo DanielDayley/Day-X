89b5b167015c5d903cf9b8e758d7ec4460730d2e
Daniel Platt <humblerolling@yahoo.com>
2015-05-04 15:50:17 -0600

    Completed add a textfield to the view controller
    lesson 5

---
+++ DayX/DetailViewController.h
@@ -0,0 +1,20 @@
+//
+//  DetailViewController.h
+//  DayX
+//
+//  Created by Monica Platt on 5/4/15.
+//  Copyright (c) 2015 DevMountain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+
+@interface DetailViewController : UIViewController <UITextFieldDelegate>
+
+@property (weak, nonatomic) IBOutlet UITextField *textField;
+
+
+
+
+@end
---
+++ DayX/DetailViewController.m
@@ -0,0 +1,44 @@
+//
+//  DetailViewController.m
+//  DayX
+//
+//  Created by Monica Platt on 5/4/15.
+//  Copyright (c) 2015 DevMountain. All rights reserved.
+//
+
+#import "DetailViewController.h"
+
+@interface DetailViewController ()
+
+@end
+
+@implementation DetailViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+-(BOOL)textFieldShouldReturn:(UITextField *)textField
+{
+    [textField resignFirstResponder];
+    return YES;
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
